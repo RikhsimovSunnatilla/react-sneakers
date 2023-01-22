703beb64a6f4b3ecd30b7901472220addac9c8f0
RikhsimovSunnatilla <rixsimovsunnatilla@gmail.com>
2023-01-22 18:57:16 +0500

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="px-16 py-9">Home</div>;
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1080px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+};
