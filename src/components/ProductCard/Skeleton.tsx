f8d5053b6e40a60cdfe85bc6c0a94f6a08ccf1ee
RikhsimovSunnatilla <rixsimovsunnatilla@gmail.com>
2023-02-07 19:01:50 +0500

    fetched products from api

--- src/components/ProductCard/Skeleton.tsx
+++ src/components/ProductCard/Skeleton.tsx
@@ -2,19 +2,21 @@ import ContentLoader from 'react-content-loader';
 
 function Skeleton() {
   return (
-    <ContentLoader
-      speed={1}
-      width={150}
-      height={188}
-      viewBox="0 0 150 188"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#ecebeb">
-      <rect x="0" y="0" rx="10" ry="10" width="150" height="90" />
-      <rect x="0" y="106" rx="3" ry="3" width="150" height="15" />
-      <rect x="0" y="125" rx="3" ry="3" width="93" height="15" />
-      <rect x="0" y="162" rx="8" ry="8" width="80" height="24" />
-      <rect x="118" y="154" rx="8" ry="8" width="32" height="32" />
-    </ContentLoader>
+    <div className="w-56 rounded-3xl border border-solid border-gray-200 bg-white px-8 pt-5 pb-8">
+      <ContentLoader
+        speed={1}
+        width={150}
+        height={188}
+        viewBox="0 0 150 188"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb">
+        <rect x="0" y="0" rx="10" ry="10" width="150" height="90" />
+        <rect x="0" y="106" rx="3" ry="3" width="150" height="15" />
+        <rect x="0" y="125" rx="3" ry="3" width="93" height="15" />
+        <rect x="0" y="162" rx="8" ry="8" width="80" height="24" />
+        <rect x="118" y="154" rx="8" ry="8" width="32" height="32" />
+      </ContentLoader>
+    </div>
   );
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 import ProductCard from '../../components/ProductCard';
+import Skeleton from '../../components/ProductCard/Skeleton';
 
-import { fetchProducts } from '../../services/product';
+import { Product } from '../../types/Product.1';
 
 function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   useEffect(() => {
-    const products = fetchProducts();
-    console.log(products);
+    async function fetchProducts() {
+      setIsLoading(true);
+
+      try {
+        const res = await axios.get(`${process.env.REACT_APP_PUBLIC_URL}/products`);
+
+        setProducts(res.data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchProducts();
   }, []);
+
+  const productCards = useMemo(
+    () => products.map((product) => <ProductCard key={product.id} {...product} />),
+    [products],
+  );
+  const skeletons = useMemo(() => [...new Array(12)].map((_, i) => <Skeleton key={i} />), []);
 
   return (
     <div className="px-11 py-9">
       <Header />
 
-      <div className="grid grid-cols-4 gap-9">
-        {Array.from({ length: 10 })
-          .fill(null)
-          .map((_, i) => (
-            <ProductCard key={i} />
-          ))}
-      </div>
+      <div className="grid grid-cols-4 gap-9">{isLoading ? skeletons : productCards}</div>
     </div>
   );
 }
